2c4325c0967418512d60a1d97c4a4f5677c55802
Fasalu Rahman <fasalukm@gmail.com>
2020-06-13 21:03:18 +0200

    build updated

---
+++ build/precache-manifest.b4132d07df89c94ceae88cfb2e759760.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39eb89e361031021b8efcfbffe5eda65",
+    "url": "/ghpages-test/index.html"
+  },
+  {
+    "revision": "776f246361103642af48",
+    "url": "/ghpages-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4e41cd457d0554a164b",
+    "url": "/ghpages-test/static/js/2.79da6aab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghpages-test/static/js/2.79da6aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776f246361103642af48",
+    "url": "/ghpages-test/static/js/main.d4988102.chunk.js"
+  },
+  {
+    "revision": "29a7cbfcef98ee79e2a6",
+    "url": "/ghpages-test/static/js/runtime-main.bbec4d79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
